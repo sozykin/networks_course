bdf73cf612900e88a416cd3755a0616fdbeef9ba
Andrey Sozykin <sozykin@gmail.com>
2025-08-09 19:54:03 +0300

    Create server.py

---
+++ sockets/server.py
@@ -0,0 +1,12 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('192.168.0.1', 8888))
+s.listen(5)
+conn, addr = s.accept()
+while True:
+    data = conn.recv(1024)
+    if not data: 
+        break
+    conn.sendall(data)
+conn.close()
+
