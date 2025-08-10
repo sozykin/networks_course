28400cb9924905c9f1cc94593dad0a7d1751e0a1
Andrey Sozykin <sozykin@gmail.com>
2025-08-10 19:49:12 +0300

    Update server.py and client.py

--- sockets/client.py
+++ sockets/client.py
@@ -1,8 +1,12 @@
 import socket
 
+server_host = '127.0.0.1'
+server_port = 8888
+
 s = socket.socket(socket.AF_INET, 
                   socket.SOCK_STREAM)
-s.connect(('192.168.0.1', 8888))
+
+s.connect((server_host, server_port))
 s.sendall(b'Hello, world!')
 data = s.recv(1024)
 s.close()
--- sockets/server.py
+++ sockets/server.py
@@ -1,6 +1,10 @@
 import socket
+
+host = '127.0.0.1'
+port = 8888
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.0.1', 8888))
+s.bind((host, port))
 s.listen(5)
 conn, addr = s.accept()
 while True:
