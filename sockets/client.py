0f8e51590790940c90973236ab50931f912d2116
Andrey Sozykin <sozykin@gmail.com>
2025-08-14 14:24:13 +0300

    Insert comments into client.py

--- sockets/client.py
+++ sockets/client.py
@@ -1,13 +1,19 @@
 import socket
 
+# Адрес и порт сервера, к которому будет выполняться подключение
 server_host = '127.0.0.1'
 server_port = 8888
 
+# Создаем сокет, протоколы IP и TCP
 s = socket.socket(socket.AF_INET, 
                   socket.SOCK_STREAM)
-
+# Подключаемся к серверу
 s.connect((server_host, server_port))
+# Отправляем данные на сервер
 s.sendall(b'Hello, world!')
+# Получаем данные от сервера
 data = s.recv(1024)
+# Закрываем сокет
 s.close()
-print('Полученные данные:', repr(data))
+# Печатаем данные, которые получили от сервера
+print('Полученные данные:', data)
